cd54a901434b128852e01c5ae5171897b9e48386
Lina Huang <huan2384@umn.edu>
2023-02-24 19:23:51 -0600

    #line 14-16

--- R/week6.R
+++ R/week6.R
@@ -10,3 +10,7 @@ citations<-stri_read_lines("../data/citations/citations.txt")
 citations_txt<-citations_txt <- citations[citations != ""] #may be redo it later
 paste(length(citations) - length(citations_txt))
 paste(mean(nchar(citations_txt)))
+
+#Data Cleaning
+set.seed(1)
+sample(citations_txt, 10)
