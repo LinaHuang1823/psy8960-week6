695ee4e0dedb419bf7fdaa6d60af2eba527eafdb
Lina Huang <huan2384@umn.edu>
2023-02-24 20:50:05 -0600

    line 14 -24

--- R/week6.R
+++ R/week6.R
@@ -1,8 +1,8 @@
 #Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(stringi)
-
-
+library(dplyr)
+library(stringr) 
 
 
 # Data Import
@@ -14,3 +14,14 @@ paste(mean(nchar(citations_txt)))
 #Data Cleaning
 set.seed(1)
 sample(citations_txt, 10)
+citations_tbl <- tibble(line = as.integer(1:length(citations_txt)), cite = citations_txt)%>%
+  mutate(cite = str_remove_all(cite, "[\'\"]"))%>%
+  mutate(year = str_extract(cite, "\\b\\d{4}\\b"))%>%
+  mutate(page_start = str_extract(cite, "\\b\\d+-"))%>% #need to edit later, no- involve 
+  mutate(perf_ref = grepl("performance", cite, ignore.case = TRUE))%>%
+  mutate(title = sub(".*\\(\\d{4}\\)\\s*\\.?\\s*([^\\.\\)]+)\\)?\\..*", "\\1", cite))%>% #need further edit
+  mutate(first_author = str_extract(cite, "(?<=^|, )\\p{Lu}\\p{Ll}+, \\p{Lu}(?=\\.)"))
+#need further edit
+#mutate(first_author = str_extract(cite, "^[A-Za-z].*?, "))
+
+
