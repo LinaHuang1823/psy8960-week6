d9bdd5cc6391f79ab6188dfc7367c1ab5d87bdf1
Lina Huang <huan2384@umn.edu>
2023-02-24 19:20:56 -0600

    line 1-12

---
+++ R/week6.R
@@ -0,0 +1,12 @@
+#Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(stringi)
+
+
+
+
+# Data Import
+citations<-stri_read_lines("../data/citations/citations.txt")
+citations_txt<-citations_txt <- citations[citations != ""] #may be redo it later
+paste(length(citations) - length(citations_txt))
+paste(mean(nchar(citations_txt)))
